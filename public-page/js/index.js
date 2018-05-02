ebfd543750b01efaf387984311bc98f0c471169d
TodayCode <onetoday1030@gmail.com>
2018-05-02 05:52:18 -0500

    updated

---
+++ public-page/js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  $("#pentity").click(function () {
+    $('#pentity').before('<div class="al-com al-2 sep">.........................</div><div class="al-com al-3"> <small>Link Type : In</small> <div class="al-combo dropdown"> <div class="first dropbtn"> <i class="fas fa-link" style="color: #A2E7C4;"></i> <i class="fas fa-caret-down"></i> </div><div class="dropdown-content"> <a href="#"> <i class="fas fa-link"></i>AND</a> <a href="#"> <i class="fas fa-link"></i>OR</a> </div><p>In</p><i class="fas fa-cog hvr-pulse-shrink"></i> </div><p>AND/OR</p></div><div class="al-com al-4 sep">.........................</div><div class="al-com al-5"> <small>Entity 1</small> <div class="al-combo dropdown"> <div class="first dropbtn"> <i class="fas fa-crosshairs"></i> <i class="fas fa-caret-down"></i> </div><div class="dropdown-content"> <a href="#"> <i class="fas fa-user"></i>Entity 1</a> <a href="#"> <i class="fas fa-crosshairs"></i>Entity 2</a> <a href="#"> <i class="fas fa-pencil-alt"></i>Entity 3</a> </div><p>Chicago</p><i class="fas fa-cog hvr-pulse-shrink"></i> </div><p>AND/OR</p></div>')
+
+  });
+});
